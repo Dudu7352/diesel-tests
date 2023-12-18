c6989d51ad764cd880c281fa1f057bf01e2624e3
Dudu7352 <aeiou7352@gmail.com>
2023-12-18 08:54:18 +0100

    generate schema, create connect method and models

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
+use diesel::{sqlite::SqliteConnection, Connection};
+use dotenvy::dotenv;
+
+pub mod schema;
+pub mod models;
+
+pub fn connect() -> SqliteConnection {
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("No DATABASE_URL set");
+    SqliteConnection::establish(&db_url).expect("Cannot connect to the specified database")
+}
+
 fn main() {
     println!("Hello, world!");
 }
